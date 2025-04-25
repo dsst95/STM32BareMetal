3f1e028408aa2df59f16aa6ca90ffa37f2295624
Dennis Stumm <ds695sas@gmail.com>
2025-04-25 08:31:23 +0200

    Added clang-tidy to cmake

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -23,5 +23,39 @@ set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-#set(CMAKE_C_CLANG_TIDY clang-tidy -allow-no-checks -extra-arg=--target=arm-none-eabi)
-#set(CMAKE_CXX_CLANG_TIDY clang-tidy -allow-no-checks -extra-arg=--target=arm-none-eabi)
+# Configure clang-tidy with includes from GNU ARM toolchain
+# Get list of include paths (https://stackoverflow.com/a/59068162/5840652)
+execute_process(
+  COMMAND ${CMAKE_CXX_COMPILER} -Wp,-v -x c++ - -fsyntax-only
+  TIMEOUT 1
+  ERROR_VARIABLE ARM_NONE_EABI_INC_DIRS
+  OUTPUT_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# Add include paths
+separate_arguments(ARM_NONE_EABI_INC_DIRS_LIST NATIVE_COMMAND
+                   ${ARM_NONE_EABI_INC_DIRS})
+foreach(DIR ${ARM_NONE_EABI_INC_DIRS_LIST})
+  string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" CONTAINS_VERSION ${DIR})
+  if(NOT ${CONTAINS_VERSION} STREQUAL "")
+    list(APPEND CXX_CLANG_EXTRA_INCLUDES -extra-arg=-isystem${DIR})
+  endif()
+endforeach()
+
+execute_process(
+  COMMAND ${CMAKE_C_COMPILER} -Wp,-v - -fsyntax-only
+  TIMEOUT 1
+  ERROR_VARIABLE ARM_NONE_EABI_INC_DIRS
+  OUTPUT_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# Add include paths
+separate_arguments(ARM_NONE_EABI_INC_DIRS_LIST NATIVE_COMMAND
+                   ${ARM_NONE_EABI_INC_DIRS})
+foreach(DIR ${ARM_NONE_EABI_INC_DIRS_LIST})
+  string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" CONTAINS_VERSION ${DIR})
+  if(NOT ${CONTAINS_VERSION} STREQUAL "")
+    list(APPEND CLANG_EXTRA_INCLUDES -extra-arg=-isystem${DIR})
+  endif()
+endforeach()
+
+set(CMAKE_C_CLANG_TIDY clang-tidy -allow-no-checks -extra-arg=--target=arm-none-eabi ${CLANG_EXTRA_INCLUDES})
+set(CMAKE_CXX_CLANG_TIDY clang-tidy -allow-no-checks -extra-arg=--target=arm-none-eabi ${CXX_CLANG_EXTRA_INCLUDES})
